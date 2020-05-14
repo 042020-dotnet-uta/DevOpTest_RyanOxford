c7e1bfe14bc7d5c9f63590fbe699380bcba017ed
roxfordlc <roxfordlc@gmail.com>
2020-05-14 15:37:48 -0500

    updating repo

--- pipelineMvcDemo/Controllers/HomeController.cs
+++ pipelineMvcDemo/Controllers/HomeController.cs
@@ -33,5 +33,10 @@ namespace pipelineMvcDemo.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public void testMethod()
+        {
+
+        }
     }
 }
